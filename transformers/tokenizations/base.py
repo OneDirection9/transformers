c456710ee4988941d5cbfde64588606d94314c57
Zhipeng Han <hanzhipeng9@gmail.com>
2020-09-21 17:26:04 +0800

    [feat]: Add batch_pad_sentence
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- transformers/tokenizations/base.py
+++ transformers/tokenizations/base.py
@@ -5,6 +5,8 @@ from abc import ABCMeta, abstractmethod
 from enum import Enum
 from typing import List, Optional, Tuple, Union
 
+import torch
+
 __all__ = ['BaseTokenizer']
 
 logger = logging.getLogger(__name__)
@@ -112,6 +114,61 @@ def truncate_sentence(
             )
 
     return ids, pair_ids, overflowing_tokens
+
+
+def batch_pad_sentence(
+    batch_ids: List[torch.Tensor],
+    padding_value: int,
+    max_length: Optional[int] = None,
+    padding_strategy: Union[str, PaddingStrategy] = PaddingStrategy.DO_NOT_PAD,
+    padding_side: str = 'right',
+    pad_to_multiple_of: Optional[int] = None,
+) -> Union[List[torch.Tensor], torch.Tensor]:
+    """Pads a batch of encoded inputs up to predefined length or to the max sequence length.
+
+    Args:
+        batch_ids: A list of 1D tensor and each tensor is the tokenized input ids.
+        padding_value: The value to make the ``batch_ids`` the same size.
+        max_length: Maximum length of the sequences.
+        padding_strategy: PaddingStrategy to use for padding:
+            * "LONGEST": Pad to the longest sequence in the batch.
+            * "MAX_LENGTH": Pad to the predefined length specified by ``max_length``.
+            * "DO_NOT_PAD": Do not pad.
+        padding_side: The side of ``padding_value`` to be added on:
+            * "left": Pads on the left of the sequences.
+            * "right": Pads on the right of the sequences.
+        pad_to_multiple_of: Integer if set will pad the sequence to a multiple of the provided
+            value. This is especially useful to enable the use of Tensor Core on NVIDIA hardware
+            with compute capability >= 7.5 (Volta).
+
+    Returns:
+        The padded batch of sequences.
+    """
+    if not isinstance(padding_strategy, PaddingStrategy):
+        padding_strategy = PaddingStrategy(padding_strategy)
+
+    if padding_strategy == PaddingStrategy.DO_NOT_PAD:
+        return batch_ids
+
+    if padding_strategy == PaddingStrategy.LONGEST:
+        max_length = max([x.size(0) for x in batch_ids])
+
+    if max_length is not None and pad_to_multiple_of is not None:
+        max_length = (
+            (max_length + pad_to_multiple_of - 1) // pad_to_multiple_of * pad_to_multiple_of
+        )
+
+    padded_ids = batch_ids[0].new_full((len(batch_ids), max_length), padding_value)
+
+    for i, ids in enumerate(batch_ids):
+        if padding_side == 'left':
+            dst = padded_ids[i][max_length - len(ids):]
+        elif padding_side == 'right':
+            dst = padded_ids[i][:len(ids)]
+        else:
+            raise ValueError(f"padding_side should be either 'left' or 'right'. Got {padding_side}")
+        dst.copy_(ids)
+    return padded_ids
 
 
 class BaseTokenizer(object, metaclass=ABCMeta):
