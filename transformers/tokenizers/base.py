58319381bb415b4fb5b1265f2dab03b169ab5d6b
Zhipeng Han <hanzhipeng9@gmail.com>
2020-10-29 17:58:40 +0800

    Sync: modifying BaseTokenizer
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- transformers/tokenizers/base.py
+++ transformers/tokenizers/base.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 from typing import Dict, List, Union
 
 __all__ = ['BaseTokenizer']
@@ -22,6 +23,40 @@ class BaseTokenizer(object, metaclass=ABCMeta):
 
     # Should be override by sub-class to specify class attributes' name related to special tokens
     SPECIAL_TOKENS_ATTRIBUTES = []
+
+    def __init__(self, symbols: List[str]) -> None:
+        """Initializes tokenizer by list of symbols."""
+        self._vocab = OrderedDict([(k, v) for v, k in enumerate(symbols)])
+        self._inv_vocab = OrderedDict([(v, k) for v, k in enumerate(symbols)])
+
+    @property
+    def vocab(self) -> Dict[str, int]:
+        return self._vocab
+
+    @property
+    def vocab_size(self) -> int:
+        """Returns the number of symbols in the dictionary."""
+        return len(self._vocab)
+
+    def __len__(self) -> int:
+        """Alias of vocab_size."""
+        return self.vocab_size
+
+    def add_tokens(self, new_tokens: List[str]) -> int:
+        """Adds a list of new tokens to the tokenizer class. If the new tokens are not in the
+        vocabulary, they are added to it with indices starting from length of the current
+        vocabulary.
+
+        Returns:
+            int: Number of tokens added to the vocabulary.
+        """
+        tokens_to_add = []
+        for token in new_tokens:
+            if token not in self._vocab:
+                logger.info(f'Adding {token} to the vocabulary')
+                tokens_to_add.append(token)
+
+        return len(tokens_to_add)
 
     @property
     def special_tokens_map(self) -> Dict[str, str]:
@@ -64,10 +99,6 @@ class BaseTokenizer(object, metaclass=ABCMeta):
         for attr in self.SPECIAL_TOKENS_ATTRIBUTES:
             if not hasattr(self, attr):
                 raise AttributeError(f"{self.__class__.__name__} does't have attribute {attr}")
-
-    @abstractmethod
-    def __len__(self) -> int:
-        pass
 
     @abstractmethod
     def tokenize(self, text: str) -> List[str]:
