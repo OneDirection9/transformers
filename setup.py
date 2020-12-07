fe6fc3a1b2d0a5363b802321bf1534c219ffeb4a
Zhipeng Han <hanzhipeng9@gmail.com>
2020-12-07 16:51:36 +0800

    Using black to format code
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- setup.py
+++ setup.py
@@ -5,42 +5,48 @@ from setuptools import find_packages, setup
 
 
 def get_version():
-    init_py_path = osp.join(osp.abspath(osp.dirname(__file__)), 'transformers', '__init__.py')
-    with open(init_py_path, 'r') as f:
-        version_line = [l.strip() for l in f.readlines() if l.startswith('__version__')][0]
-    version = version_line.split('=')[-1].strip().strip('"\'')
+    init_py_path = osp.join(osp.abspath(osp.dirname(__file__)), "transformers", "__init__.py")
+    with open(init_py_path, "r") as f:
+        version_line = [l.strip() for l in f.readlines() if l.startswith("__version__")][0]
+    version = version_line.split("=")[-1].strip().strip("\"'")
     return version
 
 
 def get_readme():
-    with open('README.md', 'r') as f:
+    with open("README.md", "r") as f:
         content = f.read()
     return content
 
 
-install_requires = [
-]
+install_requires = []
 
 extras_require = {
+    "dev": [
+        "flake8==3.8.4",
+        "isort==5.6.4",
+        "black==20.8b1",
+        "flake8-bugbear",
+        "flake8-comprehensions",
+    ]
 }
 
 setup(
-    name='transformers',
+    name="transformers",
     version=get_version(),
-    description='Transformers for Natural Language Processing',
+    description="Transformers for Natural Language Processing",
     long_description=get_readme(),
-    keywords='Transformers',
-    packages=find_packages(include=('transformers.*',)),
+    keywords="Transformers",
+    packages=find_packages(include=("transformers.*",)),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
     ],
-    author='Zhipeng Han',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    author="Zhipeng Han",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     install_requires=install_requires,
     extras_require=extras_require,
 )
