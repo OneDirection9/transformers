c4657d010fbe9d90373ae9815a7f46d97b01c311
Zhipeng Han <hanzhipeng9@gmail.com>
2020-12-11 16:37:41 +0800

    Sync with foundation
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- transformers/utils/file_io.py
+++ transformers/utils/file_io.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-from foundation.common.file_io import HTTPURLHandler, OneDrivePathHandler, PathManagerBase
+from foundation.common.file_io import HTTPURLHandler, OneDrivePathHandler, PathManagerFactory
 
 __all__ = ["PathManager"]
 
-PathManager = PathManagerBase()
+# Get per-project PathManager
+PathManager = PathManagerFactory.get("transformers_path_manager")
 
 PathManager.register_handler(HTTPURLHandler())
 PathManager.register_handler(OneDrivePathHandler())
