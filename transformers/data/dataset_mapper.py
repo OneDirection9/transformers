81462c7f09604533825d2b33de5ec1aaa2e64a43
Zhipeng Han <hanzhipeng9@gmail.com>
2021-02-08 10:30:54 +0800

    Add NoOp DatasetMapper
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- transformers/data/dataset_mapper.py
+++ transformers/data/dataset_mapper.py
@@ -1,25 +1,30 @@
 from __future__ import absolute_import, division, print_function
 
+from typing import Dict
+
+import torch
+
 from transformers.config import configurable
-from transformers.tokenizers import Tokenizer, build_tokenizer
 
 
 class DatasetMapper(object):
     """
     A callable which takes a dataset dict and map it into a format used by the model.
+
+    Mapper can add more randomization at each step.
     """
 
     @configurable
-    def __init__(self, tokenizer: Tokenizer, is_train: bool = True) -> None:
-        self.tokenizer = tokenizer
+    def __init__(self) -> None:
+        pass
 
     @classmethod
-    def from_config(cls, cfg, is_train: bool = True) -> dict:
-        tokenizer = build_tokenizer(cfg)
-        return {
-            "tokenizer": tokenizer,
-            "is_train": is_train,
-        }
+    def from_config(cls, cfg, is_train=True) -> dict:
+        return {}
 
-    def __call__(self, dataset_dict: dict) -> dict:
+    def __call__(self, dataset_dict: Dict[str, torch.Tensor]) -> dict:
+        """
+        Args:
+            dataset_dict: input_ids, token_type_ids, special_tokens_mask, next_sent_label
+        """
         return dataset_dict
